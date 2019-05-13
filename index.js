c2d696b20dcd286eb3674daddfcf5ad3234680fb
Nick Timman <>
2019-05-13 18:15:23 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-var midi = require('midi');
+const midi = require('midi');
 
 // Set up a new input.
-var input;
+let input;
 
-// Count the available input ports.
-let deviceName	= 'WORLDE easy pad';
+// Device name (to find the device in list of MIDI devices)
+const deviceName	= 'WORLDE easy pad';
+const padIndexes = [39, 48, 45, 43, 51, 49, 36, 38, 40, 42, 44, 46];
+const padNumber = padIndexes.length;
 
-let STATUS_SLIDER = 240;
-let STATUS_PAD = 153;
-let STATUS_CONTROL = 185;
+const STATUS_SLIDER = 240;
+const STATUS_PAD = 153;
+const STATUS_CONTROL = 185;
 
-let CONTROL_REFRESH = 49;
-let CONTROL_PREVIOUS = 47;
-let CONTROL_NEXT = 48;
-let CONTROL_RECORD = 44;
-let CONTROL_STOP = 46;
-let CONTROL_PLAY = 45;
+const CONTROL_REFRESH = 49;
+const CONTROL_PREVIOUS = 47;
+const CONTROL_NEXT = 48;
+const CONTROL_RECORD = 44;
+const CONTROL_STOP = 46;
+const CONTROL_PLAY = 45;
 
-let EVENT_OPEN = 'open';
-let EVENT_CLOSE = 'close';
-let EVENT_PAD_DOWN  = 'pad down';
-let EVENT_PAD_UP  = 'pad up';
-let EVENT_BUTTON_DOWN  = 'button down';
-let EVENT_BUTTON_UP  = 'button up';
-let EVENT_BAR_UPDATE  = 'update';
-let EVENT_PAGE_CHANGE = 'change page';
+const EVENT_OPEN = 'open';
+const EVENT_CLOSE = 'close';
+const EVENT_PAD_DOWN  = 'pad down';
+const EVENT_PAD_UP  = 'pad up';
+const EVENT_PAGED_PAD_DOWN  = 'page pad down';
+const EVENT_PAGED_PAD_UP  = 'page pad up';
+const EVENT_BUTTON_DOWN  = 'button down';
+const EVENT_BUTTON_UP  = 'button up';
+const EVENT_BAR_UPDATE  = 'update';
+const EVENT_PAGE_CHANGE = 'change page';
+
+let tbarForward	= true;
+let page = 0;
+
+let _eventListeners = {};
 
 
-var tbarDirection = 1;
-var tbarForward	= true;
-
-var _eventListeners = {};
-
-
-
+/**
+ *
+ */
 function open()
 {
     input = new midi.input();
-    var length = input.getPortCount();
-    var deviceIndex	= -1;
-    for(var i=0; i<length; i++)
+    const length = input.getPortCount();
+    let deviceIndex	= -1;
+    for(let i=0; i<length; i++)
     {
-    	if(input.getPortName(i) == deviceName) {
+    	if(input.getPortName(i) === deviceName)
+    	{
     		deviceIndex = i;
     		break;
     	}
@@ -68,6 +74,11 @@ function open()
     }
 }
 
+/**
+ *
+ * @param {number} deltaTime
+ * @param {Array<number>} message
+ */
 function handleMessage(deltaTime, message)
 {
     // The message is an array of numbers corresponding to the MIDI bytes:
@@ -75,46 +86,50 @@ function handleMessage(deltaTime, message)
     // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
     // information interpreting the messages.
     // console.log('m:' + message + ' d:' + deltaTime);
-    
-    var status = message[0];
+
+    const status = message[0];
     
     switch(status)
     {
         case STATUS_SLIDER:
-            var subStatus = message[1];
+            const subStatus = message[1];
             switch(subStatus)
             {
                 case 127:
-                    var tbarValue = (message[6] / 127);
-                    var bouncedValue = tbarForward ? tbarValue : (1 - tbarValue);
+                    let tbarValue = (message[6] / 127);
+                    let bouncedValue = tbarForward ? tbarValue : (1 - tbarValue);
                     
                     dispatchEvent(EVENT_BAR_UPDATE, bouncedValue);
                     
-                    if(tbarValue == 1)
+                    if(tbarValue === 1)
                         tbarForward = false;
-                    if(tbarValue == 0)
+                    if(tbarValue === 0)
                         tbarForward = true;
                 break;
                 case 66:
-                    var page = message[9];
+                    page = message[9];
                     dispatchEvent(EVENT_PAGE_CHANGE, page);
                 break;
             }
             break;
             
         case STATUS_PAD:
-            var padDown = message[2] > 0;
-            var padIndex = translatePadIndex(message[1]);
+            const padDown = message[2] > 0;
+            const padIndex = translatePadIndex(message[1]);
             
-            if(padIndex > -1)
+            if(padIndex > -1) {
                 dispatchEvent(padDown ? EVENT_PAD_DOWN : EVENT_PAD_UP, padIndex);
+
+                const padIndexWithPage = (page * padNumber) + padIndex;
+                dispatchEvent(padDown ? EVENT_PAGED_PAD_DOWN : EVENT_PAGED_PAD_UP, padIndexWithPage);
+            }
             else
                 dispatchEvent(padDown ? EVENT_BUTTON_DOWN : EVENT_BUTTON_UP, message[1]);
         break;
         
         case STATUS_CONTROL:
-            var controlDown = message[2] > 0;
-            var controlIndex = message[1];
+            const controlDown = message[2] > 0;
+            const controlIndex = message[1];
             
             if(controlDown)
             {
@@ -142,7 +157,9 @@ function handleMessage(deltaTime, message)
 }
 
 
-
+/**
+ *
+ */
 function close()
 {
 	// Close the port when done.
@@ -152,10 +169,14 @@ function close()
 }
 
 
+/**
+ *
+ * @param {number} n
+ * @return {number}
+ */
 function translatePadIndex(n)
 {
-	var indexes	= [39, 48, 45, 43, 51, 49, 36, 38, 40, 42, 44, 46];
-	var index	= indexes.indexOf(n);
+	const index	= padIndexes.indexOf(n);
 	
 	if(index > -1)
 		return index + 1;
@@ -163,12 +184,22 @@ function translatePadIndex(n)
 	return -1;
 }
 
+/**
+ *
+ * @param {string} event
+ * @param {function} callback
+ */
 function on(event, callback)
 {
     _eventListeners[event]  = callback;
 }
 
-function dispatchEvent(event, arguments)
+/**
+ *
+ * @param {string} event
+ * @param {null|Array|number} arguments
+ */
+function dispatchEvent(event, arguments=null)
 {
     if(_eventListeners[event])
     {
